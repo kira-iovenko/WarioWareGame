127d1d272151dd4187a1a5ffa5edf27f93d73c7a
Kira Iovenko <103902421+kira-iovenko@users.noreply.github.com>
2026-09-01 00:22:28 -0400

    Add scripts

---
+++ my-cool-wario-ware-game/Scenes/minigame_2.gd
@@ -0,0 +1,25 @@
+extends Node2D
+@onready var themed_timer: Node2D = $ThemedTimer
+
+var buttons_pressed = 0
+var timer_end = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await themed_timer.Timer(7.0)
+	timer_end = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if buttons_pressed == 4:
+		if Global.minigames_done > 3:
+			get_tree().change_scene_to_file("res://Scenes/dome_screen.tscn")
+		else:
+			get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
+
+	if timer_end:
+		Global.lives -= 1
+		Global.minigames_done -= 1
+		get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
