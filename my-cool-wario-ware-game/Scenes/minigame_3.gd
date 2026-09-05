d364eb9a9bd6c72ecb2cb11582a4ce1164ad3d77
Kira Iovenko <103902421+kira-iovenko@users.noreply.github.com>
2026-09-04 23:32:05 -0400

    Add maze minigame

---
+++ my-cool-wario-ware-game/Scenes/minigame_3.gd
@@ -0,0 +1,34 @@
+extends Node2D
+@onready var themed_timer: Node2D = $ThemedTimer
+
+var garlic_collected = 0
+var timer_end = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Garlic1.garlic_collected.connect(garlic_collect)
+	$Garlic2.garlic_collected.connect(garlic_collect)
+	$Garlic3.garlic_collected.connect(garlic_collect)
+	await themed_timer.Timer(10.0)
+	timer_end = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if garlic_collected == 3:
+		if Global.minigames_done > 3:
+			get_tree().change_scene_to_file("res://Scenes/done_screen.tscn")
+		else:
+			get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
+	
+	if timer_end:
+		Global.minigames_done -= 1
+		Global.lives -= 1
+		if Global.lives > 0:
+			get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
+		else:
+			get_tree().change_scene_to_file("res://Scenes/fail_screen.tscn")
+
+func garlic_collect() -> void:
+	garlic_collected += 1
+	return
