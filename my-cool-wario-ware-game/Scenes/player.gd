41264dbfd7f7c2a39128ad613532f0078249127d
Kira Iovenko <103902421+kira-iovenko@users.noreply.github.com>
2026-09-07 17:26:15 -0400

    Fix player sprite direction

--- my-cool-wario-ware-game/Scenes/player.gd
+++ my-cool-wario-ware-game/Scenes/player.gd
@@ -19,6 +19,10 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction < 0:
+			$Sprite2D.flip_h = true
+		elif direction > 0:
+			$Sprite2D.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
