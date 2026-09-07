e16dd3f118ded5065f6919eaae228690ae76d622
Kira Iovenko <103902421+kira-iovenko@users.noreply.github.com>
2026-09-07 17:33:38 -0400

    Fix top-down player sprite direction

--- my-cool-wario-ware-game/Scenes/player_topdown.gd
+++ my-cool-wario-ware-game/Scenes/player_topdown.gd
@@ -8,5 +8,15 @@ func _physics_process(delta: float) -> void:
 
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * SPEED
+	
+	if direction:
+		if direction.x < 0:
+			$Sprite2D.rotation_degrees = 90
+		elif direction.x > 0:
+			$Sprite2D.rotation_degrees = -90
+		elif direction.y < 0:
+			$Sprite2D.rotation_degrees = 180
+		elif direction.y > 0:
+			$Sprite2D.rotation_degrees = 0
 
 	move_and_slide()
