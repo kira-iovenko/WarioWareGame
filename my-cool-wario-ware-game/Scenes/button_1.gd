2a3b3a2c121ddcaf7850d7eabf699d1e6e3efd59
Kira Iovenko <103902421+kira-iovenko@users.noreply.github.com>
2026-09-07 17:47:25 -0400

    Make stars move randomply

--- my-cool-wario-ware-game/Scenes/button_1.gd
+++ my-cool-wario-ware-game/Scenes/button_1.gd
@@ -1,15 +1,23 @@
 extends TextureButton
 @onready var parent = $".."
 
+var speed: float
+var direction: Vector2
+var change_direction_time: float
+
 func _on_pressed() -> void:
 	hide()
 	parent.buttons_pressed += 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	speed = randf_range(100, 200)
+	direction = Vector2.from_angle(randf_range(0, 360)).normalized()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position += direction * speed * delta
+	if position.x < 0 or position.x > 950:
+		direction.x *= -1
+	if position.y < 0 or position.y > 450:
+		direction.y *= -1
